9dfde5d75ee2d32c2eb7aa272f81bc28ef1bbbe7
murito-sudo <65056899+murito-sudo@users.noreply.github.com>
2023-08-11 01:52:04 -0400

    Flexible MongoDB URI

--- src/main/java/com/example/socialMediaAPI/SocialMedia_API/MongoDBConfiguration.java
+++ src/main/java/com/example/socialMediaAPI/SocialMedia_API/MongoDBConfiguration.java
@@ -4,8 +4,10 @@ import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 
 import com.mongodb.ConnectionString;
@@ -14,13 +16,23 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
 @Configuration
-public class MongoDBConfiguration {
-	@Autowired
-    private Environment env;
+public class MongoDBConfiguration  {
+
+		
+    	private Environment env;
 	
-	 @Bean
+		@Autowired
+	    public MongoDBConfiguration(Environment env) {
+	        this.env = env;
+	    }
+
+		
+	    @Bean
 	    public MongoClient mongoClient() {
-	        String mongoUri = env.getProperty("SPRING_DATA_MONGODB_URI");  // Replace with your MongoDB URI
+	        String mongoUri = env.getProperty("SPRING_DATA_MONGODB_URI") == null ? "mongodb://localhost:27017/social-media-api" : env.getProperty("SPRING_DATA_MONGODB_URI"); // Replace with your MongoDB URI
+		
+	        
+	        System.out.println("This is: " + mongoUri);
 	        
 	        CodecRegistry pojoCodecRegistry = CodecRegistries.fromProviders(
 	                PojoCodecProvider.builder().automatic(true).build());
@@ -35,6 +47,23 @@ public class MongoDBConfiguration {
 	                .build();
 
 	        return MongoClients.create(settings);
+	    }
+	    
+	    @EventListener(ApplicationReadyEvent.class)
+	    public void performMongoDBConfiguration() {
+	        // Here you can add additional MongoDB-specific configuration
+	        // after the application is fully initialized.
+	        // For example, you might want to set indexes or other options.
+
+	        // Access the mongoClient bean you've defined above
+	        MongoClient mongoClient = mongoClient();
+
+	        // You can use the mongoClient to perform configuration tasks
+	        // on your MongoDB instance as needed.
+	        // For example:
+	        // mongoClient.getDatabase("your-database-name")
+	        //     .getCollection("your-collection-name")
+	        //     .createIndex(Indexes.ascending("fieldName"));
 	    }
 	
 	
